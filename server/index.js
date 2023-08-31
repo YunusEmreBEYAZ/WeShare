c523b4e275933c2e347a049d343c3f3eabb01b7a
Yunus Emre BEYAZ <yebeyaz@gmail.com>
2023-08-31 13:56:19 +0200

    posts route

--- server/index.js
+++ server/index.js
@@ -8,9 +8,13 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
-import userRoutes from "./routes/users.js";
+import { verifyToken } from "./middleware/auth.js";
+import { createPost } from "./controllers/posts.js";
+
 
 
 //CONFIGURATION
@@ -43,11 +47,14 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-// ROUTES
-app.post("/auth/register", upload.single("picture"), register)
+// ROUTES WITH FILES
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost)
 
+//ROUTES
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 
 //MONGOOSE setup
