0853a706f62cf845d4346292b97a03c43d6aa477
Yunus Emre BEYAZ <yebeyaz@gmail.com>
2023-09-16 00:34:05 +0200

    reducer created

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
-import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "scenes/homePage";
-//import Navbar from "scenes/navbar";
 import LoginPage from "scenes/loginPage";
 import ProfilePage from "scenes/profilePage";
 
