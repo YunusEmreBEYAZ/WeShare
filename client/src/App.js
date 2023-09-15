9db27507608bccad589cd3037695e46b423922a8
Yunus Emre BEYAZ <yebeyaz@gmail.com>
2023-09-16 00:18:26 +0200

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
-
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+//import Navbar from "scenes/navbar";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
 
 
 function App() {
   return (
     <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<LoginPage />} />
+          <Route path="/" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
