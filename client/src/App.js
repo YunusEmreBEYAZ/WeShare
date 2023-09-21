f7230037ecf20d2d19ba946f9a2591815092be44
Yunus Emre BEYAZ <yebeyaz@gmail.com>
2023-09-21 18:59:49 +0200

    fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "scenes/homePage";
 import LoginPage from "scenes/loginPage";
 import ProfilePage from "scenes/profilePage";
@@ -9,9 +9,10 @@ import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "./theme";
 
 function App() {
-
   const mode = useSelector((state) => state.mode);
-  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
+
   return (
     <div className="app">
       <BrowserRouter>
@@ -19,13 +20,18 @@ function App() {
           <CssBaseline />
           <Routes>
             <Route path="/" element={<LoginPage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/profile/:userId" element={<LoginPage />} />
-            <Route path="/" element={<ProfilePage />} />
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
           </Routes>
         </ThemeProvider>
       </BrowserRouter>
-    </div >
+    </div>
   );
 }
 
--- client/src/components/FlexBetween.jsx
+++ client/src/components/FlexBetween.jsx
@@ -1,11 +1,10 @@
 import { Box } from "@mui/material";
 import { styled } from "@mui/system";
 
-// THIS COMPONENT FOR CSS
 const FlexBetween = styled(Box)({
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center"
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
 });
 
 export default FlexBetween;
